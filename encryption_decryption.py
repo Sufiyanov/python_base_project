dad88bc429d6b9ac4612f8f3534dd0879fc49132
Rushan <31798659+Sufiyanov@users.noreply.github.com>
2022-03-22 15:55:03 +0300

    Update encryption_decryption.py

--- encryption_decryption.py
+++ encryption_decryption.py
@@ -1,9 +1,8 @@
-eng_lower_alphabet = 'abcdefghijklmnopqrstuvwxyz'
-eng_upper_alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 rus_lower_alphabet = "абвгдежзийклмнопрстуфхцчшщъыьэюя"
 rus_upper_alphabet = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
 
-# Функция шифрования 
+
+# Функция шифрования
 def encryption(text, step):
     result = ''
     for i in text:
@@ -20,3 +19,29 @@ def encryption(text, step):
         else:
             result += i
     return result
+
+
+# Функция расшифрования
+def decryption(text, step):
+    result = ''
+    for i in text:
+        if i in rus_lower_alphabet:
+            if rus_lower_alphabet.find(i) - step >= 0:
+                result +=  rus_lower_alphabet[rus_lower_alphabet.find(i) - step]
+            else:
+                result += rus_lower_alphabet[(rus_lower_alphabet.find(i) - step + len(rus_lower_alphabet))]
+        elif i in rus_upper_alphabet:
+            if rus_upper_alphabet.find(i) - step >= 0:
+                result +=  rus_upper_alphabet[rus_upper_alphabet.find(i) - step]
+            else:
+                result += rus_upper_alphabet[(rus_upper_alphabet.find(i) - step + len(rus_upper_alphabet))]
+        else:
+            result += i
+    return result
+
+
+text = 'Умом Россию не понять'
+text1 = 'Фнпн Спттйя ож рпоауэ'
+
+print(encryption(text, 1))
+print(decryption(text1, 1))
